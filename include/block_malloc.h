d0415774deb9b3053e63202f46301925abf5ddb9
lipeng <734991033@qq.com>
2025-09-05 00:41:57 +0800

    comment(block_malloc):增加注释

--- include/block_malloc.h
+++ include/block_malloc.h
@@ -1,3 +1,8 @@
+/*
+block_malloc 是一个专为管理固定大小的内存块（blocks）而设计的内存分配器。
+它模拟了简单的内存池机制，支持高效的分配和释放操作，支持线程安全，保持api简洁性
+*/
+
 #ifndef BLOCK_MALLOC_H
 #define BLOCK_MALLOC_H
 
@@ -63,13 +68,48 @@ typedef struct
     _Atomic int64_t lock;  // 原子锁，用于多线程同步
 } blocks_meta_t;
 
+/**
+ * 初始化块管理器元数据。
+ * 检查总内存大小是否至少能容纳一个block_t结构体。
+ * 初始化所有字段为默认值：块计数为0，空闲链表为空，锁为未锁定.
+ */
 int blocks_init(blocks_meta_t *meta, const uint64_t total_size, const uint64_t block_size);
+
+/**
+ * 分配一个块。
+ * 检查空闲链表：若有空闲块则重用，否则检查内存是否足够分配新块。
+ * 更新元数据计数和块状态，返回块block_id或-1（失败）。
+ */
 int64_t blocks_alloc(blocks_meta_t *meta, void *block_start);
+/**
+ * 释放一个块。
+ * 验证块ID有效性，检查块是否已分配。
+ * 更新块状态并将其添加回空闲链表，更新已用块计数.
+ */
 void blocks_free(blocks_meta_t *meta, void *block_start, const uint64_t block_id);
 
+/*
+辅助计算函数
+计算对应块id，相对block_start的bytes偏移量
+*/
 int64_t block_offset(const blocks_meta_t *meta, const uint64_t block_id);
+
+/*
+ * 辅助计算函数
+ * 计算对应块id的data起始地址，相对block_start的bytes偏移量
+ */
 int64_t block_data_offset(const blocks_meta_t *meta, const uint64_t block_id);
+
+/*
+ * 辅助计算函数
+ * 从block相对block_start的bytes偏移量，计算对应块id
+ */
 int64_t block_id_byblockoffset(const blocks_meta_t *meta, const uint64_t block_offset);
+
+/*
+ * 辅助计算函数
+ * 从blockdata相对block_start的bytes偏移量，计算对应块id
+ */
 int64_t block_id_bydataoffset(const blocks_meta_t *meta, const uint64_t data_offset);
 
 #endif // BLOCK_MALLOC_H
